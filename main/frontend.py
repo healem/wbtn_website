d5bc5915a910858b89f374299625cd84ca7b097c
Mike Healey <healem@alum.rpi.edu>
2016-08-14 18:07:27 -0500

    Wired in flask and python

---
+++ main/frontend.py
@@ -0,0 +1,55 @@
+#!../bin/python
+import ConfigParser
+from utils import loginit
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+loginit.initTestLogging()
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Buy groceries',
+        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Learn Python',
+        'description': 'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+#@app.route('/authorize/<provider>')
+#def oauth_authorize(provider):
+#    oauth = Social.get_provider(provider)
+#    return oauth.authorize()
+
+#@app.route('/callback/<provider>')
+#def oauth_callback(provider):
+#    oauth = Social.get_provider(provider)
+#    socialId, email, firstName, lastName = oauth.callback()
+#    if email is None:
+#        return None
+#    user = dbm.getUserByEmail(email)
+    #if not user:
+    #    dbm.addNormalUser(email=testEmail, facebookId=socialId, firstName=firstName, lastName=lastName)
+    #    user = dbm.getUserByEmail(email)
+        
+#    return user
+
+if __name__ == '__main__':
+    loginit.initLogging()
+    app.run(debug=True)
+
---
+++ main/utils/loginit.py
@@ -0,0 +1,72 @@
+#!../../bin/python
+
+import logging
+import logging.config
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file':{
+            'class': 'logging.handlers.RotatingFileHandler',
+            'mode': 'a',
+            'maxBytes': 10485760,
+            'backupCount': 5,
+            'level':'DEBUG',
+            'formatter': 'default',
+            'filename': '/home4/healem/logs/wbtn/wbtn.log',
+            'encoding': 'utf-8',
+        },
+        'console': {
+                'class': 'logging.StreamHandler',
+                'level': 'INFO',
+                'formatter': 'default',
+        },
+    },
+    'root': {
+        'handlers': ['file', 'console'],
+        'level': 'DEBUG',
+    },
+}
+
+TEST_LOGGING = {
+'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s %(name)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file':{
+            'class': 'logging.handlers.RotatingFileHandler',
+            'mode': 'a',
+            'maxBytes': 10485760,
+            'backupCount': 5,
+            'level':'DEBUG',
+            'formatter': 'default',
+            'filename': '/home4/healem/logs/wbtn/wbtn-test.log',
+            'encoding': 'utf-8',
+        },
+        'console': {
+                'class': 'logging.StreamHandler',
+                'level': 'DEBUG',
+                'formatter': 'default',
+        },
+    },
+    'root': {
+        'handlers': ['file', 'console'],
+        'level': 'DEBUG',
+    },
+}
+
+def initLogging():
+    logging.config.dictConfig(LOGGING)
+
+def initTestLogging():
+    logging.config.dictConfig(TEST_LOGGING)
